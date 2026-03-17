cdcb586a0f2030f7d3f27ca10d8d4348a469d179
georgii <goqwertys@gmail.com>
2026-03-17 16:22:22 +0400

    Init project with users app

---
+++ users/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'is_active', 'is_staff')
---
+++ users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
+
+from .views import RegisterView, MeView
+
+urlpatterns = [
+    path("auth/register/", RegisterView.as_view()),
+    path("auth/login/", TokenObtainPairView.as_view()),
+    path("auth/refresh/", TokenRefreshView.as_view()),
+    path("auth/me/", MeView.as_view()),
+]
