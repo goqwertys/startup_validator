7504a03122b7d1f329904884c6dfb513a39ee5e1
georgii <goqwertys@gmail.com>
2026-03-18 02:29:22 +0400

    admin filter added

--- ideas/admin.py
+++ ideas/admin.py
@@ -1,8 +1,34 @@
+from datetime import timedelta
+
 from django.contrib import admin
 from django.db.models import Sum
 from django.db.models.functions import Coalesce
+from django.utils import timezone
 
 from ideas.models import Idea
+
+
+class TopIdeasFilter(admin.SimpleListFilter):
+    title = 'Top ideas'
+    parameter_name = 'top'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('top', 'Top 10'),
+            ('week', 'Top this week')
+        )
+
+    def queryset(self, request, queryset):
+        queryset = queryset.annotate(score=Coalesce(Sum('votes__value'), 0))
+
+        if self.value() == 'top':
+            return queryset.order_by('-score')[:10]
+
+        if self.value() == 'week':
+            week_ago = timezone.now() - timedelta(days=7)
+            return queryset.filter(created_at__gte=week_ago).order_by('-score')
+
+        return queryset
 
 
 @admin.register(Idea)
@@ -19,6 +45,6 @@ class IdeaAdmin(admin.ModelAdmin):
 
     score.admin_order_field = 'score'
 
-    list_filter = ('category', 'created_at')
+    list_filter = ('category', 'created_at', TopIdeasFilter)
     search_fields = ('title', 'description')
     ordering = ('-created_at',)
