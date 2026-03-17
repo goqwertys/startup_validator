d9bd9232688e2964ff3bb11af47418ccab218130
georgii <goqwertys@gmail.com>
2026-03-17 17:54:32 +0400

    add idea viewset

--- ideas/views.py
+++ ideas/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from ideas.models import Idea
+from ideas.serializers import IdeaSerializer
+
+
+class IdeaViewSet(ModelViewSet):
+
+    queryset = Idea.objects.all().order_by('-created_at')
+    serializer_class = IdeaSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
