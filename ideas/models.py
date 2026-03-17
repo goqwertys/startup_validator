a9ee13240424564c901c55bccb41a8cd652c608d
georgii <goqwertys@gmail.com>
2026-03-17 17:46:15 +0400

    add idea model

--- ideas/models.py
+++ ideas/models.py
@@ -1,3 +1,30 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Idea(models.Model):
+
+    class Category(models.TextChoices):
+        TECH = "tech", "Tech"
+        AI = "ai", "AI"
+        FINANCE = "finance", "Finance"
+        EDUCATION = "education", "Education"
+        OTHER = "other", "Other"
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField(
+        max_length=50,
+        choices=Category.choices,
+        default=Category.OTHER
+    )
+
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='ideas'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
