37b45b26bdd8218358fce28a35cd1e58b0fdc313
georgii <goqwertys@gmail.com>
2026-03-18 02:37:57 +0400

    add period filtering for ideas

--- ideas/filters.py
+++ ideas/filters.py
@@ -1,3 +1,7 @@
+from datetime import timedelta
+
+from django.utils import timezone
+
 import django_filters
 from .models import Idea
 
@@ -5,6 +9,20 @@ from .models import Idea
 class IdeaFilter(django_filters.FilterSet):
     category = django_filters.CharFilter()
     creator = django_filters.NumberFilter(field_name='creator__id')
+
+    period = django_filters.CharFilter(method='filter_period')
+
+    def filter_period(self, queryset, name, value):
+
+        if value == 'week':
+            week_ago = timezone.now() - timedelta(days=7)
+            return queryset.filter(created_at__gte=week_ago)
+
+        if value == 'deay':
+            day_ago = timezone.now() - timedelta(days=1)
+            return queryset.filter(created_at__gte=day_ago)
+
+        return queryset
 
     class Meta:
         model = Idea
