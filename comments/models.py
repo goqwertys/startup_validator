52e7f3be6b6331660d3686bd5a3fb1faa94eb0a8
georgii <goqwertys@gmail.com>
2026-03-18 03:59:10 +0400

    app installed with basic comment model

---
+++ comments/models.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.db import models
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+
+    idea = models.ForeignKey(
+        'ideas.Idea',
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} -> {self.idea}'
