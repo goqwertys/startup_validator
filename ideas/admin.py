5789a96981ae419ecb4116759949642dae347359
georgii <goqwertys@gmail.com>
2026-03-17 22:19:19 +0400

    add owner permissions for ideas

--- ideas/admin.py
+++ ideas/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from ideas.models import Idea
+
+
+@admin.register(Idea)
+class IdeaAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'title', 'category', 'creator', 'created_at')
+    list_filter = ('category', 'created_at')
+    search_fields = ('title', 'description')
+    ordering = ('-created_at',)
--- ideas/views.py
+++ ideas/views.py
@@ -2,6 +2,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from ideas.models import Idea
+from ideas.permissions import IsOwnerOrReadOnly
 from ideas.serializers import IdeaSerializer
 
 
@@ -9,7 +10,7 @@ class IdeaViewSet(ModelViewSet):
 
     queryset = Idea.objects.all().order_by('-created_at')
     serializer_class = IdeaSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
