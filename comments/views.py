5b0c8cfedbbf8934504b575de1e374af528b5f13
georgii <goqwertys@gmail.com>
2026-03-18 04:13:15 +0400

    comment viewset added

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Comment
+from .permissions import IsOwnerOrReadOnly
+from .serializers import CommentSerializer
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["idea"]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
