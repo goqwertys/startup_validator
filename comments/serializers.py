52aa5df9fcd96d5cf4c7fc67e3eef2b4d684fb49
georgii <goqwertys@gmail.com>
2026-03-19 21:50:20 +0400

    add nested replies serializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -3,9 +3,24 @@ from rest_framework import serializers
 from comments.models import Comment
 
 
+class RecursiveSerializer(serializers.ModelSerializer):
+    def to_representation(self, value):
+        serializer = CommentSerializer(value, context=self.context)
+        return serializer.data
+
+
 class CommentSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.email')
+    replies = RecursiveSerializer(many=True, read_only=True)
 
     class Meta:
         model = Comment
-        fields = ('id', 'idea', 'user', 'text', 'created_at')
+        fields = (
+            'id',
+            'idea',
+            'user',
+            'text',
+            'parent',
+            'replies',
+            'created_at'
+        )
